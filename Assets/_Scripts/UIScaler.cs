0eb5cc8fdd016fa586bd71f36bb880d5244d2d0c
CutebatDev <alexaverten@gmail.com>
2025-12-04 13:47:37 +0200

    Some scale fixes

---
+++ Assets/_Scripts/UIScaler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Scripts
+{
+    public class UIScaler : MonoBehaviour
+    {
+        private CanvasScaler canvas;
+
+        private void Awake()
+        {
+            canvas = gameObject.GetComponent<CanvasScaler>();
+            canvas.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        }
+
+        private void Update()
+        {
+            canvas.referenceResolution = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+        }
+    }
+}
